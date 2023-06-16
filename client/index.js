b8bb537a26cd5010b5a7aebbf5dfa935ce44a312
keshav2802 <keshavkr2802@gmail.com>
2023-06-16 11:54:24 +0530

    updated server url

--- client/index.js
+++ client/index.js
@@ -78,7 +78,7 @@ async function handleSubmit(e) {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-9qgs.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
